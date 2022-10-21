fc5f91558f74d70c116d7740cad862cb8860f3c2
raidanttraining <radianttraining@outlook.com>
2022-10-21 04:47:20 +0000

    add ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "myec2" {
+    ami = "ami-026b57f3c383c2eec"
+    instance_type = var.ec2type
+    tags = local.common_tags
+
+  }
+
+
+locals {
+  # Common tags to be assigned to all resources..
+  common_tags = {
+    Service = "appservice"
+    Owner   = "birlasoft"
+  }
+}
+
+
+variable "ec2type" {
+    description = "this is my ec2 instance type"
+    type = string
+    default = "t3.medium"
+}
+
+output "ec2_ip" {
+  value = aws_instance.myec2.public_ip
+} 
